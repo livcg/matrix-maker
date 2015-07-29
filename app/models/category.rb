6710d70897339f92e3f5bf7c225c64ae9ff91623
Liv Galendez <livcg1@gmail.com>
2015-07-29 18:07:14 +1000

    Fixed deletion of matrices

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :matrix
-  has_many :options
+  has_many :options, dependent: :destroy
 
   validates :name, presence: true,
 				 length: { minimum: 1 }
