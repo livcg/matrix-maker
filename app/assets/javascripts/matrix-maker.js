fa8cf9d9b4e534aba02c2b1bf1393220a78bda25
Liv Galendez <livcg1@gmail.com>
2015-07-31 11:36:15 +1000

    Display previous moves in matrix

--- app/assets/javascripts/matrix-maker.js
+++ app/assets/javascripts/matrix-maker.js
@@ -1,32 +1,50 @@
 
 
 
+getMovesUrl = location.href + "/moves"
+addMoveUrl = location.href + "/moves"
+
 $(document).ready(function() {
+	//*** Delete?
     for (groupIndex = 2; groupIndex <= 5; groupIndex++) {
-	var val
+		var val
 
-	// Dynamically set column group labels based on row group labels
+		// Dynamically set column group labels based on row group labels
 
-	val = $("input.label.group" + groupIndex).val()
-	$("td.label.group" + groupIndex).text(val)
+		val = $("input.label.group" + groupIndex).val()
+		$("td.label.group" + groupIndex).text(val)
 
-	$("input.label.group" + groupIndex).change( function() {
-	    var val = $(this).val()
-	    $("td.label." + this.classList[0]).text(val) // TODO: Check for relevant class
-	})
+		$("input.label.group" + groupIndex).change( function() {
+		    var val = $(this).val()
+		    $("td.label." + this.classList[0]).text(val) // TODO: Check for relevant class
+		})
 
-	// Dynamically set column labels based on row labels
-	
-	for (itemIndex = 1; itemIndex <= 5; itemIndex++) {
-	    val = $("input.label.g" + groupIndex + "i" + itemIndex).val()
-	    $("td.label.g" + groupIndex + "i" + itemIndex).html(val) //*** note text()?
+		// Dynamically set column labels based on row labels
+		
+		for (itemIndex = 1; itemIndex <= 5; itemIndex++) {
+		    val = $("input.label.g" + groupIndex + "i" + itemIndex).val()
+		    $("td.label.g" + groupIndex + "i" + itemIndex).html(val) //*** note text()?
 
-	    $("input.label.g" + groupIndex + "i" + itemIndex).change( function() {
-		var val = $(this).val()
-		$("td.label." + this.classList[0]).html(val) // TODO: Check for relevant class
-	    })
-	}
+		    $("input.label.g" + groupIndex + "i" + itemIndex).change( function() {
+			var val = $(this).val()
+			$("td.label." + this.classList[0]).html(val) // TODO: Check for relevant class
+		    })
+		}
     }
+
+    // Replay moves
+    $.getJSON(getMovesUrl, function() {
+    	console.log("success") 
+    }).done( function(data) {
+    	$.each(data, function(i, move) {
+	    	symbol = move[2]
+	    	if (symbol != null) {
+		    	moveId = move[0]
+		    	cellId = move[1]
+		    	$("td#" + cellId).text(symbol)
+	    	}
+		})
+	})
 
     // Toggle cells between 'X', 'O', 'X?', 'O?', and ''
     var symbols = [ 'X', 'O', 'X?', 'O?', '' ]
@@ -41,8 +59,7 @@ $(document).ready(function() {
 		    }
 		}
 		newSymbol = symbols[newSymbolIndex]
-		moveUrl = location.href + "/moves"
-		$.ajax({ url: moveUrl,
+		$.ajax({ url: addMoveUrl,
 			   	method: "POST",
 			   	data: { move: { cell: $(this).attr("id"), symbol: newSymbol }}})
 			.done(function() { 
--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,10 +1,17 @@
 class MovesController < ApplicationController
+  def index
+    @matrix = Matrix.find(params[:matrix_id])
+    @moves = @matrix.moves.sort_by{ |move| move.created_at }
+    	.collect { |move| [ move.id, move.cell, move.symbol ]}
+    render json: @moves
+  end	
+ 
   def create
     @matrix = Matrix.find(params[:matrix_id])
     @move = @matrix.moves.create(move_params)
     redirect_to matrix_path(@matrix)
   end
- 
+
   private
     def move_params
       params.require(:move).permit(:cell, :symbol)
