1acc240b718de193425c06e43c5cadae482f3e9a
Liv Galendez <livcg1@gmail.com>
2015-09-11 14:13:48 +1000

    More code cleanup

--- app/assets/javascripts/matrix-maker.js
+++ app/assets/javascripts/matrix-maker.js
@@ -61,8 +61,7 @@ $(document).ready(function() {
 		if ((cellId != undefined) && (cellId.length > 0) && (symbol != undefined) && (symbol != null)) { //*** ick
 			//*** console.log("Move: " + cellId + " : " + symbol)
 			$("td#" + cellId).text(symbol)
-//***			addToMoveListOnPage(moveId, cellId, symbol, moveArrIndex)
-			addToMoveListOnPage2(moveId, cellId, symbol, moveArrIndex)
+			addToMoveListOnPage(moveId, cellId, symbol, moveArrIndex)
 		} 
 	}
 
@@ -89,8 +88,7 @@ $(document).ready(function() {
 		tdElement.html(newSymbol)
 
 		// Update move list on the page
-//***		moveCount = addToMoveListOnPage(DEFAULT_MOVE_ID, cellId, newSymbol, moveArrIndex)
-		moveCount = addToMoveListOnPage2(DEFAULT_MOVE_ID, cellId, newSymbol, moveArrIndex)
+		moveCount = addToMoveListOnPage(DEFAULT_MOVE_ID, cellId, newSymbol, moveArrIndex)
 
 		// Update server
 		$.post(ADD_MOVE_URL,
@@ -142,62 +140,16 @@ function isNoteMove(cellId) {
 	return (cellId == NOTE_CELL_ID)
 }
 
+// ***?:
 // Regular move:
-//   <span id="m<MOVE_COUNT>i<MOVE_ID>"><span class="move-count"><MOVE_COUNT></span>: <CELL_ID> : <SYMBOL></span><br/>
+//   <tr id="m<MOVE#>i-1"> <td class="move-count"><MOVE_COUNT></td> <td><CELL_ID></td> <td><SYMBOL></td> </tr>
 //   =>
-//   <span id="m<MOVE_COUNT>i<MOVE_ID>"><a ...><MOVE_COUNT></a>: <CELL_ID> : <SYMBOL></span><br/>
+//   <tr id="m<MOVE#>i<CELL_ID>"> <td class="move-count"><a ...><MOVE_COUNT></a></td> <td><CELL_ID></td> <td><SYMBOL></td> </tr>
 // Note:
-//   <span id="n<MOVE_ARRAY_INDEX>i<MOVE_ID>"><span class="move-count">Note</span>: <SYMBOL></span><br/> 
+//   <tr id="m?i?"> <td class="?"><MOVE_COUNT></td> <td><CELL_ID></td> <td><SYMBOL></td> </tr>
 //   =>
-//   <span id="n<MOVE_ARRAY_INDEX>i<MOVE_ID>"><a ...>Note</a>: <SYMBOL></span><br/> 
+//   <tr id="m?i?"> <td class="?"><a ...>Note</a></td> <td colspan="2"><NOTE></td> </tr>
 function addToMoveListOnPage(moveId, cellId, symbol, moveArrIndex) {
-	textPrefix = ""
-	string = ""
-
-	// Defaults for a regular move (not a note)
-	moveCount = moveCounter
-	spanId = getMoveSpanId(moveCount, moveId, cellId, moveArrIndex)
-
-	// If needed, add new <tr> or <td>
-	if (addMoveTableRow) {
-		$("table#moves span.last-count").removeClass("last-count")
-		$("table#moves").append("<tr><th colspan=\"" + moveTableHeadingColspan + "\">Moves " + moveCount + 
-			"-<span class=\"last-count\"></span>:</th></tr><tr></tr>")
-		addMoveTableRow = false
-	}
-	if (addMoveTableColumn) {
-		$("table#moves tr:last-of-type").append("<td></td>")
-		addMoveTableColumn = false		
-	}
-	if (isNoteMove(cellId)) {
-		// Note rather than a regular move
-		// textPrefix = "<a href=\"" + UNDO_MOVES_URL + "/" + moveId 
-		// 	+ "\" data-confirm=\"Are you sure you want to remove this note ("
-		// 	+ symbol + ") and all moves after it?\" data-method=\"post\">Note</a>: "
-		textPrefix = getMoveNumberHtml(moveCount, moveId, cellId, symbol) + ": "
-	} else {
-		// Regular move
-		textPrefix = getMoveNumberHtml(moveCount, moveId, cellId, symbol) + ": " + cellId + " : "
-		moveCounter++
-		if ((moveCounter > 1) && (moveCounter % MAX_MOVES_PER_ROW == 1))
-			addMoveTableRow = true
-		if ((moveCounter > 1) && (moveCounter % MAX_MOVES_PER_COLUMN == 1))
-			addMoveTableColumn = true
-	}
-	string = "<span id=\"" + spanId + "\">" + textPrefix + symbol + "</span><br/>"
-	$("table#moves tr:last-of-type td:last-of-type").append(string)
-
-	// Update last move in current row's <th>
-	$("table#moves span.last-count").text(moveCount) //*** Optimize this on page load
-
-	return moveCount
-}
-
-// Regular move:
-//   <td class="move-count"><MOVE_COUNT></td> <td><CELL_ID></td> <td><SYMBOL></td>
-// Note:
-//   <td>Note</td> <td colspan="2"><NOTE></td>
-function addToMoveListOnPage2(moveId, cellId, symbol, moveArrIndex) {
 	// Defaults for a regular move (not a note)
 	moveCount = moveCounter
 	moveTrId = getMoveTrId(moveCount, moveId, cellId, moveArrIndex)
